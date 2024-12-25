7eae69757f2ace4df72e3ada8a0fa0d41ea8299a
RehenumaIlman <hridihossain382@gmail.com>
2024-12-26 03:06:43 +0600

    Add files via upload

---
+++ Pyhton project/random_madlibs.py
@@ -0,0 +1,7 @@
+# from sample_madlibs import hp, code, zombie, hungergames # type: ignore
+# import random
+
+# if __name__ == "__main__":
+#     m = random.choice([hp, code, zombie, hungergames])
+#     m.madlib() ei coe ta or 1 of the madlibs ja o toiri kre rakhse  chalanor jnno dewa
+# I'll prepare my new madlibs here!!!
